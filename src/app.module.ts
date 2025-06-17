549438dcb43499e8ee4c16e8967b2e283799c252
osoben1y <asadbekmmjnv@gmail.com>
2025-06-18 00:30:48 +0500

    Admin table have been started

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { AdminModule } from './admin/admin.module';
 
 @Module({
-  imports: [],
+  imports: [AdminModule],
 
 })
 export class AppModule {}
