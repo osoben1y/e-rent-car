362c6d4aad772285ea4f0bc5408d673aaaefc414
osoben1y <asadbekmmjnv@gmail.com>
2025-06-18 04:49:21 +0500

    created model for admin

---
+++ src/admin/models/admin.model.ts
@@ -0,0 +1,63 @@
+import { Column, DataType, Model, Table } from "sequelize-typescript";
+
+interface AdminAttr {
+    full_name:string;
+    email:string;
+    phone_number:string;
+    birthdate:Date;
+    hashed_password:string;
+    is_active:Boolean;
+    is_admin:Boolean;
+    hashed_refresh_token:string;
+}
+
+@Table({tableName: 'admin'})
+export class Admin extends Model<Admin, AdminAttr> {
+
+    @Column({
+        type: DataType.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    })
+    id: number;
+
+    @Column({
+
+    })
+    full_name:string;
+
+    @Column({
+
+    })
+    email:string;
+
+    @Column({
+
+    })
+    phone_number:string;
+
+    @Column({
+
+    })
+    birthdate:Date
+
+    @Column({
+
+    })
+    hashed_password:string
+
+    @Column({
+
+    })
+    is_active:Boolean
+
+    @Column({
+
+    })
+    is_admin: Boolean
+
+    @Column({
+
+    })
+    hashed_refresh_token:string
+}
