4b4c801a00a4841422edaba150191e8750787798
osoben1y <asadbekmmjnv@gmail.com>
2025-06-19 12:44:10 +0500

    added mail service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AdminModule } from './admin/admin.module';
+import { MailModule } from './mail/mail.module';
 
 @Module({
-  imports: [AdminModule],
+  imports: [AdminModule, MailModule],
 })
 export class AppModule {}
