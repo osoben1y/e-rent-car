f8c8308b804b6a68e6d1adfba5f5a7fa1d0cfc7d
osoben1y <asadbekmmjnv@gmail.com>
2025-06-19 13:23:33 +0500

    Added otp

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AdminModule } from './admin/admin.module';
 import { MailModule } from './mail/mail.module';
+import { OtpModule } from './otp/otp.module';
 
 @Module({
-  imports: [AdminModule, MailModule],
+  imports: [AdminModule, MailModule, OtpModule],
 })
 export class AppModule {}
---
+++ src/otp/otp.service.ts
@@ -0,0 +1,33 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { EmailCustomerDto } from './dto/email-customer.dto';
+import * as otpGenerator from 'otp-generator';
+import { InjectModel } from '@nestjs/sequelize';
+import { Otp } from './models/otp.model';
+import { AddMinutesToDate } from 'src/helpers/addMinutes';
+import { Op } from 'sequelize';
+import { dates, decode, encode } from 'src/helpers/crypto';
+import { VerifyOtpDto } from './dto/verifyOtp.dto';
+import { Customer } from 'src/customer/models/customer.model';
+
+@Injectable()
+export class OtpService {
+  create(createOtpDto: CreateOtpDto) {
+    return 'This action adds a new otp';
+  }
+
+  findAll() {
+    return `This action returns all otp`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} otp`;
+  }
+
+  update(id: number, updateOtpDto: UpdateOtpDto) {
+    return `This action updates a #${id} otp`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} otp`;
+  }
+}
